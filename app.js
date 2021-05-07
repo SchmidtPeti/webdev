3ab1b06b1dfe565720f00be8e246c3ccba031e2c
schmidt peti <schmidt.peti32@gmail.com>
2021-05-07 12:01:09 +0200

    contrast website

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const toggleStyleSheet = () => {
     let emptyStyle = document.getElementById("contrast");
     let currentPath = emptyStyle.href.substring(emptyStyle.href.lastIndexOf("/")+1,emptyStyle.href.length);
+    let btn = document.getElementById("v");
     if(currentPath!=="nincs.css"){
         emptyStyle.href="nincs.css";
+        btn.innerHTML = "Váltás nagy kontrasztos stílusra"
     }
     else{
         let van = document.location.href;
@@ -10,5 +12,6 @@ const toggleStyleSheet = () => {
         let newPath = van.substring(0,idx);
         newPath += "/bigContrast.css";
         emptyStyle.href = newPath;
+        btn.innerHTML = "Váltás normáls nézetre"
     }
 }
